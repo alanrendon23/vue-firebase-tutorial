0db7698607b4fbbfb749245b72326defd547b47d
alanrendon23 <30661018+alanrendon23@users.noreply.github.com>
2020-02-13 13:02:55 -0600

    Template login y Router
    Se modifico el router para solo aceptar las paginas basicas, se elimino la vista about y se modificaron los css en la vista de Login

---
+++ vue-firebase-tutorial/src/views/Login.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="login">
+        <h3>Sig In</h3>
+        <input type="text" placeholder="Email"><br>
+        <input type="password" placeholder="Password"><br>
+        <button>Connection</button>
+        <p>You don't have an account ? yo can create once</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'login',
+        data(){
+            return {};
+        },
+        methods: {}
+    }
+</script>
+<style scoped>
+    .login{
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 10px;
+    }
+    button {
+        margin-top: 20px;
+        width: 10%;
+        cursor: pointer;
+    }
+    p {
+        margin-top: 40px;
+        font-size: 13px;
+    }
+    p a{
+        text-decoration: underline;
+        cursor: pointer;
+    }
+</style>
