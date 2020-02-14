53af8003cb353023c558225f33cc81cc2ed5e625
alanrendon23 <30661018+alanrendon23@users.noreply.github.com>
2020-02-13 19:26:47 -0600

    Manejo de Datos
    Se creo una instancia de firebase para obtener los datos de la coleccion 'answers' y se implementaron graficas

--- vue-firebase-tutorial/src/components/HelloWorld.vue
+++ vue-firebase-tutorial/src/components/HelloWorld.vue
@@ -1,41 +1,79 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+<div class="hello">
+    <Plotly :data="data" :layout="layout" :display-mode-bar="false"></Plotly>
+</div>
+</template>  
 
 <script>
+import firebase from 'firebase';
+import { Plotly } from 'vue-plotly'
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    Plotly
+  },
+  data(){
+    var db = firebase.database();
+    var scoresRef = db.ref().child('answers');
+    var matrix = [];
+    scoresRef.on('child_added', function(snapshot) {
+      if(matrix[snapshot.val().questionId] == null){
+        matrix[snapshot.val().questionId]= [];
+        matrix[snapshot.val().questionId][0] = 0;
+        matrix[snapshot.val().questionId][1] = 0;
+      }
+      if(snapshot.val().right == true) {
+        matrix[snapshot.val().questionId][1] = (matrix[snapshot.val().questionId][1] != null) ? matrix[snapshot.val().questionId][1] +1 : 0;
+      }else{
+        matrix[snapshot.val().questionId][0] = (matrix[snapshot.val().questionId][0] != null) ? matrix[snapshot.val().questionId][0] +1 : 0;
+      }      
+    });
+    
+
+
+    //console.log(Object.values(matrix));
+    var errores = new Array();
+    var aciertos = new Array();
+    Object.keys(matrix).sort().forEach(element => {
+      errores.push(matrix[element][0]);
+      aciertos.push(matrix[element][1]);
+    });
+
+    var trace1 = {
+      x: aciertos,
+      y: Object.keys(matrix).sort(),
+      name: 'B',
+      orientation: 'h',
+      marker: {
+        color: 'rgba(42,79,143,0.6)',
+        width: 1
+      },
+      type: 'bar'
+    };
+
+    var trace2 = {
+      x: errores,
+      y: Object.keys(matrix).sort(),
+      name: 'Errores',
+      orientation: 'h',
+      type: 'bar',
+      marker: {
+        color: 'rgba(226,139,5,0.6)',
+        width: 1
+      }
+    };
+
+    var data = [trace1, trace2];
+
+
+    return {data,
+    layout:{
+      autosize: false,
+      width: 1200,
+      height: 800,
+      title: 'Aciertos y Errores Por pregunta',
+      barmode: 'stack'
+    }};
   }
 }
 </script>
--- vue-firebase-tutorial/src/views/Home.vue
+++ vue-firebase-tutorial/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
     <button @click="logout">LogOut</button>
   </div>
 </template>
